b767a23cec59799e95ba06e116422a6d555f5ed8
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-02-15 17:59:11 -0500

    more path issues

--- app/js/app_routes.js
+++ app/js/app_routes.js
@@ -13,16 +13,16 @@ define(['app', 'lodash', 'js/extended-route-provider', 'services/app-config-serv
         $locationProvider.hashPrefix('!');
         
         $routeProvider.
-               when('/',                { templateUrl: 'views/home/index.html', label:'ABSCH', resolveController: true, resolveUser : true, resolve : { securized : securize(null,null, true) }}).
+               when('/',                { templateUrl: 'views/home/index.html', label:'OASIS', resolveController: true, resolveUser : true, resolve : { securized : securize(null,null, true) }}).
                
                when('/projects',         { templateUrl: 'views/projects/index.html', label:'Projects', resolveController: true, resolveUser : true, resolve : { securized : securize(null,null, true) }}).
                
                when('/articles',         { templateUrl: 'views/article/index.html', label:'Article Editor', resolveController: true, resolveUser : true, resolve : { securized : securize(null,null, true) }}).
-               when('/articles/new',      { templateUrl: 'views/article/editor.html', label:'Article Editor', isNew:true, resolveController: true, resolveUser : true, resolve : { securized : securize(null,null, true) }}).
-               when('/articles/:id/:title?/edit', { templateUrl: 'views/article/editor.html', label:'Article Editor', resolveController: true, resolveUser : true, resolve : { securized : securize(null,null, true) }}).
+               when('/articles/new',      { templateUrl: 'views/article/editor.html', label:'Article Editor', isNew:true, resolveController: true, resolveUser : true, resolve : { securized : securize(['Administrator', 'oasisArticleEditor'],null, true) }}).
+               when('/articles/:id/:title?/edit', { templateUrl: 'views/article/editor.html', label:'Article Editor', resolveController: true, resolveUser : true, resolve : { securized : securize(['Administrator', 'oasisArticleEditor'],null, true) }}).
                when('/articles/:id/:title?',     { templateUrl: 'views/article/view.html', label:'Article Editor', resolveController: true, resolveUser : true, resolve : { securized : securize(null,null, true) }}).
               
-               when('/manage/:schema',          { templateUrl: 'views/tags/index.html', label:'Article Editor', resolveController: true, resolveUser : true, resolve : { securized : securize(null,null, true) }}).               
+               when('/manage/:schema',          { templateUrl: 'views/tags/index.html', label:'Article Editor', resolveController: true, resolveUser : true, resolve : { securized : securize(['Administrator', 'oasisArticleEditor'],null, true) }}).               
                when('/signin',          { templateUrl: 'views/shared/login.html', label:'Article Editor', resolveController: true, resolveUser : true, resolve : { securized : securize(null,null, true) }}).               
                
                
@@ -44,22 +44,6 @@ define(['app', 'lodash', 'js/extended-route-provider', 'services/app-config-serv
 
                 var roles = _.clone(roleList||[]);
 
-                if (roles && !_.isEmpty(roles)) {
-                    roles = _.map(roles, appConfigService.getRoleName);
-                }
-                if(useNationalRoles){
-                    var path = $location.$$url.replace('/register/','');
-                    var schema;
-
-                    if(path.indexOf('/')>0)
-                        schema = path.substr(0, path.indexOf('/'));
-                    else
-                        schema = path;
-
-                    var schemaName = $filter('mapSchema')(schema);
-                    if(!_.contains(appConfigService.referenceSchemas, schemaName))
-                        roles = (roles || []).concat(appConfigService.nationalRoles());
-                }
                 if (!user.isAuthenticated) {
 
                     console.log("securize: force sign in");
