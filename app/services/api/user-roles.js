8ab2227bc4fc8a495c27fd0b869603d9e8a31912
Blaise Fonseca <blaise.fonseca@un.org>
2024-08-13 10:35:28 -0400

    use params to pass query string

--- app/services/api/user-roles.js
+++ app/services/api/user-roles.js
@@ -8,7 +8,12 @@ export default class UserRolesApi extends ApiBase
   }
 
   async getUserRoleNames(roleCodes)  {
-    return this.http.get(`api/v2013/roles?q={"roles":${JSON.stringify(roleCodes)}}`)
+    return this.http.get(`api/v2013/roles`, 
+                    {
+                      params : {
+                        q : JSON.stringify({"roles":roleCodes?.map(encodeURIComponent)})
+                      }
+                    })                    
                     .then(res => res.data)
                     .catch(tryCastToApiError);
   }
