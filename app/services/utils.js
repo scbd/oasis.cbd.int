f8c40f63a81f78000b8a2d1960851ac49d717168
Blaise Fonseca <blaise.fonseca@un.org>
2024-08-23 16:45:21 -0400

    Add option to show diff in api and solr count

--- app/services/utils.js
+++ app/services/utils.js
@@ -9,3 +9,64 @@ export function isRealm(realm, realmToTest, strict) {
     
     return realmRe.test(realmToTest);
 }
+
+
+export function escape(value) {
+
+    if(value===undefined) return;
+    if(value===null)      return;
+    if(value==="")        return;
+
+    if(_.isNumber(value)) value = value.toString();
+    if(_.isDate  (value)) value = value.toISOString();
+
+    //TODO add more types
+
+    value = value.toString();
+
+    value = value.replace(/\\/g,   '\\\\');
+    value = value.replace(/\+/g,   '\\+');
+    value = value.replace(/\-/g,   '\\-');
+    value = value.replace(/\&\&/g, '\\&&');
+    value = value.replace(/\|\|/g, '\\||');
+    value = value.replace(/\!/g,   '\\!');
+    value = value.replace(/\(/g,   '\\(');
+    value = value.replace(/\)/g,   '\\)');
+    value = value.replace(/\{/g,   '\\{');
+    value = value.replace(/\}/g,   '\\}');
+    value = value.replace(/\[/g,   '\\[');
+    value = value.replace(/\]/g,   '\\]');
+    value = value.replace(/\^/g,   '\\^');
+    value = value.replace(/\"/g,   '\\"');
+    value = value.replace(/\~/g,   '\\~');
+    value = value.replace(/\*/g,   '\\*');
+    value = value.replace(/\?/g,   '\\?');
+    value = value.replace(/\:/g,   '\\:');
+
+    return value;
+}
+
+export function andOr(query, sep) {
+
+    sep = sep || 'AND';
+
+    if(Array.isArray(query)) {
+
+        query = query.map(function(criteria){
+
+            if(Array.isArray(criteria)) {
+                return andOr(criteria, sep=="AND" ? "OR" : "AND");
+            }
+
+            return criteria;
+        });
+
+        query = '(' + query.join(' ' + sep + ' ') + ')';
+    }
+
+    return query;
+}
+
+export function localizeFields (field, locale){
+    return field.replace(/_EN_/ig, `_${locale.toUpperCase()}_`);
+}
